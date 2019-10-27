d1c92d31bd670137580d1206297ec4a18db31c13
Anastasia Pankina <siapankina@gmail.com>
2019-10-27 17:20:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,7 +57,7 @@ def runner
   welcome
   card_total=initial_round 
   card_total=hit?(card_total)
-  if card_total<21
+  until card_total<21
     hit?(card_total)
     display_card_total(card_total)
   else
